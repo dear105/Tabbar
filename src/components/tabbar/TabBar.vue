2f5956ba805359151d09cf87f105cb60d97d2bcd
孙佳佳 <sunjaekko@163.com>
2020-12-10 10:22:29 +0800

    购物车项目底部Tabbar封装

---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="tab-bar">
+   <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "TabBar",
+  data() {
+    return {};
+  },
+  components: {},
+};
+</script>
+<style >
+#tab-bar {
+  display: flex;
+  background-color: rgb(212, 206, 206);
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -3px 10px rgba(100, 100, 100, 0.1);
+}
+</style>
